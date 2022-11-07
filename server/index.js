ab0ac0ef0afaf8b772026baf2368dde59789fcc3
ARK <Anantha-Kandrapu>
2022-11-07 16:45:32 -0700

    OK Working but unscaled

--- server/index.js
+++ server/index.js
@@ -1,34 +1,18 @@
-const sharp = require('sharp')
 const fs = require('fs');
 // tensorflowjs_converter--input_format keras tf_model.h5./ jsmodel /
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-
-var Jimp = require('jimp');
-
+let imagePixels = require('image-pixels')
+let Jimp = require('jimp');
 const tf = require("@tensorflow/tfjs");
 const tfn = require("@tensorflow/tfjs-node");
 const handler = tfn.io.fileSystem("./jsmodel/model.json");
 const data = require('./data');
+const { count } = require('console');
 
-const getData = async (filename) => {
 
-    let dataFileBuffer = fs.readFileSync(filename);
-    console.log('lenght', dataFileBuffer.length, 'Bytes', dataFileBuffer.BYTES_PER_ELEMENT)
-    let pixels = [];
-    for (let y = 0; y <= 27; y++) {
-        for (let x = 0; x <= 27; x++) {
-            const data = dataFileBuffer[x + (y * 28) + 16];
-            if (data == undefined)
-                pixels.push(0);
-            else
-                pixels.push(data);
-        }
-    }
-    return pixels;
-}
 
 const imageDecoder = async (pikels) => {
     try {
@@ -36,6 +20,7 @@ const imageDecoder = async (pikels) => {
         const predictionData = await data.convertPixels(pikels);
         const ans2d = model.predict(predictionData, { batchSize: 1 }).arraySync()
         const ans = ans2d[0];
+        console.log(ans, 'HEYYEY');
         return ans.lastIndexOf(1);
     }
     catch (e) {
@@ -53,19 +38,35 @@ const storage2 = multer.diskStorage({
     }
 })
 
+const getData = async (filename) => {
+    let { data, width, height } = await imagePixels(filename)
+    let pixels = [];
+    for (let i = 0; i < data.length; i = i + 4) {
+        let ag = data[i] + data[i + 1] + data[i + 2];
+        pixels.push(Math.ceil(ag / 3));
+    }
+    for (let i = 0; i < 28; ++i) {
+        for (let j = 0; j < 28; ++j) {
+            process.stdout.write(`${pixels[28 * i + j]}  `)
+        }
+        console.log();
+    }
+    console.log(pixels.length, 'Cou')
+    fs.unlinkSync(filename);
+    return pixels;
+}
 const fileToData = async (filename) => {
     const tempAddr = filename.slice(filename.lastIndexOf('/'));
     const modFileName = './uploads/temp2' + tempAddr;
     const image = await Jimp.read(filename);
     image
-        .rotate(90)
         .grayscale()
+        .threshold({ max: 130 })
+        .invert()
         .resize(28, 28)
-        .quality(60)
     await image.writeAsync(modFileName);
     console.log("image modded")
     const pixels = await getData(modFileName);
-    fs.unlinkSync(modFileName);
     return pixels;
 }
 
@@ -76,8 +77,8 @@ app.post('/upload2', upload2.single('image'), async (req, res) => {
     const pixels = await fileToData(oldPath);
     const label = await imageDecoder(pixels);
     console.log('Label : ', label);
-    let newPath = `./uploads/${label}/${FileName}`
-    fs.renameSync(oldPath,newPath);
+    let newPath = `./uploads/${Math.abs(label)}/${FileName}`
+    fs.renameSync(oldPath, newPath);
     console.log(newPath);
     res.send(JSON.stringify({ "status": "uploaded" }));
 })
@@ -87,7 +88,7 @@ console.log('listening on 3000');
 
 function createDirs() {
     for (let i = 0; i < 10; ++i) {
-        let dir = `./uploads/${i}`;
+        let dir = `./uploads/${Math.abs(i)}`;
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
         }
